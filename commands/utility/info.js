830096e8c4c9f23ab0886b612e57d2b7b2f433f3
Heyimlulu <lucas.rouillermonay@edueptm.ch>
2020-08-18 10:43:39 +0200

    aaa fix

--- commands/utility/info.js
+++ commands/utility/info.js
@@ -7,7 +7,7 @@ module.exports = {
         const Embed = new Discord.MessageEmbed()
             .setColor('#800080')
             .setTitle('Informations')
-            .setDescription('racoon bot by Ｙｕｋｉ#0001')
+            .setDescription('Racoon bot made with 💛 by Ｙｕｋｉ#0001')
             /*
             .setDescription('Made with 💛 by Ｙｕｋｉ#0001')
             .addField('racoon bot by Ｙｕｋｉ#0001', 'You can find it here at the [Racoon Bot Repository](https://github.com/Heyimlulu/racoon-bot)', true)
