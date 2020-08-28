8952d7c360b144af088b8905687e3e93542e85ed
Heyimlulu <61554619+Heyimlulu@users.noreply.github.com>
2020-08-28 19:08:56 +0200

    fix index.hbs

---
+++ commands/owner/cute.js
@@ -0,0 +1,39 @@
+const Discord = require('discord.js');
+
+module.exports = {
+    name: 'cuteometer',
+    description: `Tell a user how cute he is.`,
+    category: 'owner',
+    execute(message) {
+
+        if (message.author.id !== '265896171384340480') {
+            message.channel.send(`Sorry, you can't do that ${message.author.tag}.`)
+            return
+
+        } else {
+
+            var cute = [
+                'Not Cute',
+                'Ho hum Cute',
+                'Ok, maybe a little cute',
+                'Awesomely cute',
+                'Adorable'];
+
+            let guess = cute[Math.floor(Math.random() * (cute.length - 1) + 1)];
+
+            if (message.mentions.users.first()) {
+                const Embed = new Discord.MessageEmbed()
+                const User = message.mentions.users.first()
+
+                Embed.setTitle('Cute-o-Meter')
+                    .setDescription(`I guess ${User.tag} is **${guess}**`)
+                    .setThumbnail(User.displayAvatarURL())
+                    .setColor("RANDOM")
+
+                message.channel.send(Embed)
+            } else {
+                message.channel.send('You did not specified a user!')
+            }
+        }
+    },
+};
