59ed26ad40f2a294a9801801aaafa059ee1b818a
Heyimlulu <61554619+Heyimlulu@users.noreply.github.com>
2020-08-29 13:32:59 +0200

    set theses commands to public

---
+++ commands/fun/cute.js
@@ -0,0 +1,33 @@
+const Discord = require('discord.js');
+
+module.exports = {
+    name: 'cuteometer',
+    description: `Tell a user how cute he is.`,
+    category: 'fun',
+    execute(message) {
+
+        var cute = [
+            'Not Cute',
+            'Ho hum Cute',
+            'Ok, maybe a little cute',
+            'Awesomely cute',
+            'Adorable'];
+
+        let guess = cute[Math.floor(Math.random() * (cute.length - 1) + 1)];
+
+        if (message.mentions.users.first()) {
+            const Embed = new Discord.MessageEmbed()
+            const User = message.mentions.users.first()
+
+            Embed.setTitle('Cute-o-Meter')
+                .setDescription(`I guess **${User.tag}** is **${guess}**`)
+                .setThumbnail(User.displayAvatarURL())
+                .setColor("RANDOM")
+
+            message.delete()
+            message.channel.send(Embed)
+        } else {
+            message.channel.send(`You did not specified a user! **${message.author.tag}**`)
+        }
+    },
+};
---
+++ commands/fun/love.js
@@ -0,0 +1,27 @@
+const Discord = require('discord.js');
+
+module.exports = {
+    name: 'love',
+    description: `Calculate the love you have to another person.`,
+    category: 'fun',
+    execute(message) {
+
+        if (message.mentions.users.first()) {
+            const love = Math.random() * 100
+            const loveIndex = Math.floor(love / 10)
+            const loveLevel = '💖'.repeat(loveIndex) + '💔'.repeat(10 - loveIndex)
+
+            const User = message.mentions.users.first()
+            const loveEmbed = new Discord.MessageEmbed()
+                .setColor("RANDOM")
+                .setTitle('Love')
+                .addField(`**${message.author.tag}** loves **${User.tag}** this much`, `💟 ${Math.floor(love)}%\n\n${loveLevel}`)
+                .setThumbnail(User.displayAvatarURL())
+
+            message.channel.send(loveEmbed)
+        } else {
+            message.channel.send(`You did not specified a user! **${message.author.tag}**`)
+        }
+    },
+
+};
