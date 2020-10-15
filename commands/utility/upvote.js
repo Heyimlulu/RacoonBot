ec4c879218a15344ccf74b07384a234dac7615ed
Heyimlulu <lucas.rouillermonay@edueptm.ch>
2020-10-15 10:03:06 +0200

    Fix url image not displayed properly

--- commands/utility/upvote.js
+++ commands/utility/upvote.js
@@ -5,6 +5,7 @@ module.exports = {
     description: 'Send a link to vote for my bot!',
     execute(message) {
 
+        const attachment = new Discord.MessageAttachment('./images/racoon-for-me.jpg', 'racoon-for-me.jpg');
         const UpvoteEmbed = new Discord.MessageEmbed()
             .setColor("RANDOM")
             .setTitle('Vote for my bot')
@@ -12,7 +13,8 @@ module.exports = {
             .setDescription('You can vote for my bot if you like it!')
             // .addField('top.gg', 'https://top.gg/bot/.../vote')
             .addField('Discordbotlist.com', 'https://discordbotlist.com/bots/racoonbot/upvote')
-            .setImage('https://lh3.googleusercontent.com/proxy/jO7O_alsGy8r3uDQpJIOVxQDUjsM1Kk5C4K3PCRRQV1_Tfml2ky2-C2fbI_m444SlnnT8AQbkwx4FQPO2gZAl612sUHgCgzWxcgAwtdHIQ1YbfFdDrCyJNIYwDMqD1NlWK9JAjl504G0Wepg3zI_uygwjAmu8wPYEFI')
+            .attachFiles(attachment)
+            .setImage('attachment://racoon-for-me.jpg')
             .setTimestamp()
             .setFooter('💛 Thanks for the upvote', message.author.displayAvatarURL());
 
