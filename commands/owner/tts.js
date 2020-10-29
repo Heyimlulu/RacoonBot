89b5b39aff5e7d321baedb6f7fd5a15a560e9541
Heyimlulu <lucas.rouillermonay@edueptm.ch>
2020-10-29 17:21:39 +0100

    fix download command

--- commands/owner/tts.js
+++ commands/owner/tts.js
@@ -1,16 +1,13 @@
-// Imports the Google Cloud client library
+/*
 const textToSpeech = require('@google-cloud/text-to-speech');
-
-// Import other required libraries
+const gclient = new textToSpeech.TextToSpeechClient();
 const fs = require('fs');
-const util = require('util');
 
-// Creates a client
-const client = new textToSpeech.TextToSpeechClient();
+const config = require('../../json/config.json');
 
 module.exports = {
     name: 'tts',
-    description: `tts`,
+    description: `Return text into a text-to-speech file`,
     category: 'owner',
     execute(message) {
 
@@ -19,26 +16,36 @@ module.exports = {
             return
         }
 
-        // The text to synthesize
-        const text = 'hello, world!';
+        let text = message.content.split(`${config.prefix}tts`).join("").trim(); // Listen to user's input
 
         // Construct the request
         const request = {
             input: {text: text},
-            // Select the language and SSML voice gender (optional)
+            // Select the language and SSML Voice Gender (optional)
             voice: {languageCode: 'en-US', ssmlGender: 'NEUTRAL'},
-            // select the type of audio encoding
+            // Select the type of audio encoding
             audioConfig: {audioEncoding: 'MP3'},
         };
 
-        // Performs the text-to-speech request
-        const [response] = client.synthesizeSpeech(request);
+        // Performs the Text-to-Speech request
+        gclient.synthesizeSpeech(request, (err, response) => {
+            if (err) {
+                console.error('ERROR:', err);
+                return;
+            }
 
-        // Write the binary audio content to a local file
-        const writeFile = util.promisify(fs.writeFile);
-
-        writeFile('output.mp3', response.audioContent, 'binary');
-
-        console.log('Audio content written to file: output.mp3');
+            // Write the binary audio content to a local file
+            fs.writeFile('tts.mp3', response.audioContent, 'binary', err => {
+                if (err) {
+                    console.error('ERROR:', err);
+                    message.channel.send('An error has occured, the message is probably too long')
+                    return;
+                }
+                console.log('Audio content written to file: tts.mp3');
+                message.channel.send({files: ['./tts.mp3']})
+            });
+        });
     },
 };
+
+ */
---
+++ commands/utility/download.js
@@ -0,0 +1,42 @@
+const Discord = require('discord.js');
+const fs = require('fs')
+const youtubedl = require('youtube-dl')
+
+const config = require('../../json/config.json');
+
+module.exports = {
+    name: 'download',
+    description: 'Download videos from youtube',
+    category: "owner",
+    execute(message) {
+
+        let ytdlUrl = message.content.split(`${config.prefix}download`).join("").trim(); // Listen to user's input
+
+        if (ytdlUrl.includes("http") || ytdlUrl.includes("www")) {
+            message.channel.send('Downloading...').then(msg => {
+                video.on('end', function () {
+                    msg.delete();
+                })
+            })
+
+            let video = youtubedl(ytdlUrl); // Get the url from the user
+
+            video.pipe(fs.createWriteStream('./video.mp4')); // Create a video.mp4 file
+
+            // on error
+            video.on('error', function error(err) {
+                console.log('error 2:', err);
+                message.channel.send("An error has occured, i can't download from the link you provided.")
+            });
+
+            // on end or if download is finished
+            video.on('end', function () {
+                message.delete();
+                message.channel.send(`Downloaded by ${message.author.username}`, {files: ["./video.mp4"]})
+                    .catch(error => message.channel.send('File too big'))
+            })
+        } else {
+            message.channel.send("You need to input a valid link");
+        }
+    },
+};
