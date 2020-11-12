cbfd74b14fb3911d2c65f381bfba9326fc3dfe53
Heyimlulu <lucas.rouillermonay@edueptm.ch>
2020-11-12 15:38:15 +0100

    remove embed message for this command

--- commands/utility/invite.js
+++ commands/utility/invite.js
@@ -5,16 +5,6 @@ module.exports = {
     description: 'Get the invite link for the bot.',
     execute(message) {
 
-        const attachment = new Discord.MessageAttachment('./images/racoonbot-banner.png', 'racoonbot-banner.png');
-
-        const inviteEmbed = new Discord.MessageEmbed()
-            .setAuthor('RacoonBot')
-            .setColor("RANDOM")
-            .setTitle("You can add me from here")
-            .setURL('https://discord.com/api/oauth2/authorize?client_id=734426328002068481&permissions=268696823&scope=bot')
-            .attachFiles(attachment)
-            .setImage('attachment://racoonbot-banner.png')
-
-        message.channel.send(inviteEmbed);
+        message.channel.send("You can add me from here: https://discord.com/api/oauth2/authorize?client_id=734426328002068481&permissions=268696823&scope=bot");
     },
 };
