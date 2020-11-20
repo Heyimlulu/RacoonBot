76951688b235409b8e91af8ea37ea458f28e7b09
Heyimlulu <lucas.rouillermonay@edueptm.ch>
2020-11-20 15:34:49 +0100

    Revert "Merge branch 'master' of https://github.com/Heyimlulu/RacoonBot"
    This reverts commit cb5b71c8ee99fc5ef809f10830c0c4024d660779, reversing
    changes made to 2424db74c426eeee9e1374eacd3197ffd7c31715.

--- commands/general/codeblocks.js
+++ commands/general/codeblocks.js
@@ -1,126 +1,82 @@
-const config = require('../../json/config.json');
+const config = require('../../json/config.json')
+;
 module.exports = {
     name: 'codeblock',
     description: 'Denote a specific programming language for syntax highlighting',
     category: 'general',
     execute(message) {
 
+        // Store user input in a variable
+        let userInput = message.content.split(`${config.prefix}codeblock`).join("").trim();
+
         // Check if user use a valid format
-        //if (message.content.startsWith(`${config.prefix}codeblock`)) return message.delete().then(message.channel.send('Which language do you want to use?'));
+        if (!userInput) return message.delete().then(message.channel.send('Which format do you want to use?'));
 
         message.delete(); // Delete message before sending code blocks
 
         if (message.content.startsWith(`${config.prefix}codeblock html`)){
-            // Store user input in a variable
-            let html = message.content.split(`${config.prefix}codeblock html`).join("").trim();
             // HTML code blocks format
-            message.channel.send(`Code shared by **${message.author.tag}**\n` +
-                '```html\n' + `${html}` + '```');
+            message.channel.send('```html\n' + `${userInput}` + '```');
         }
         if (message.content.startsWith(`${config.prefix}codeblock css`)){
-            // Store user input in a variable
-            let css = message.content.split(`${config.prefix}codeblock css`).join("").trim();
             // CSS code blocks format
-            message.channel.send(`Code shared by **${message.author.tag}**\n` +
-                '```css\n' + `${css}` + '```');
+            message.channel.send('```css\n' + `${userInput}` + '```');
         }
         if (message.content.startsWith(`${config.prefix}codeblock javascript`)){
-            // Store user input in a variable
-            let javascript = message.content.split(`${config.prefix}codeblock javascript`).join("").trim();
             // Javascript code blocks format
-            message.channel.send(`Code shared by **${message.author.tag}**\n` +
-                '```javascript\n' + `${javascript}` + '```');
+            message.channel.send('```javascript\n' + `${userInput}` + '```');
         }
         if (message.content.startsWith(`${config.prefix}codeblock php`)){
-            // Store user input in a variable
-            let php = message.content.split(`${config.prefix}codeblock php`).join("").trim();
             // Php code blocks format
-            message.channel.send(`Code shared by **${message.author.tag}**\n` +
-                '```php\n' + `${php}` + '```');
+            message.channel.send('```php\n' + `${userInput}` + '```');
         }
         if (message.content.startsWith(`${config.prefix}codeblock csharp`)){
-            // Store user input in a variable
-            let csharp = message.content.split(`${config.prefix}codeblock csharp`).join("").trim();
             // C# code blocks format
-            message.channel.send(`Code shared by **${message.author.tag}**\n` +
-                '```csharp\n' + `${csharp}` + '```');
+            message.channel.send('```csharp\n' + `${userInput}` + '```');
         }
         if (message.content.startsWith(`${config.prefix}codeblock cpp`)){
-            // Store user input in a variable
-            let cpp = message.content.split(`${config.prefix}codeblock cpp`).join("").trim();
             // C++ code blocks format
-            message.channel.send(`Code shared by **${message.author.tag}**\n` +
-                '```cpp\n' + `${cpp}` + '```');
+            message.channel.send('```cpp\n' + `${userInput}` + '```');
         }
         if (message.content.startsWith(`${config.prefix}codeblock bash`)){
-            // Store user input in a variable
-            let bash = message.content.split(`${config.prefix}codeblock bash`).join("").trim();
             // Bash code blocks format
-            message.channel.send(`Code shared by **${message.author.tag}**\n` +
-                '```bash\n' + `${bash}` + '```');
+            message.channel.send('```bash\n' + `${userInput}` + '```');
         }
         if (message.content.startsWith(`${config.prefix}codeblock xml`)){
-            // Store user input in a variable
-            let xml = message.content.split(`${config.prefix}codeblock xml`).join("").trim();
             // Xml code blocks format
-            message.channel.send(`Code shared by **${message.author.tag}**\n` +
-                '```xml\n' + `${xml}` + '```');
+            message.channel.send('```xml\n' + `${userInput}` + '```');
         }
         if (message.content.startsWith(`${config.prefix}codeblock markdown`)){
-            // Store user input in a variable
-            let markdown = message.content.split(`${config.prefix}codeblock markdown`).join("").trim();
             // Markdown code blocks format
-            message.channel.send(`Code shared by **${message.author.tag}**\n` +
-                '```markdown\n' + `${markdown}` + '```');
+            message.channel.send('```markdown\n' + `${userInput}` + '```');
         }
         if (message.content.startsWith(`${config.prefix}codeblock ruby`)){
-            // Store user input in a variable
-            let ruby = message.content.split(`${config.prefix}codeblock ruby`).join("").trim();
             // Ruby code blocks format
-            message.channel.send(`Code shared by **${message.author.tag}**\n` +
-                '```ruby\n' + `${ruby}` + '```');
+            message.channel.send('```ruby\n' + `${userInput}` + '```');
         }
         if (message.content.startsWith(`${config.prefix}codeblock go`)){
-            // Store user input in a variable
-            let go = message.content.split(`${config.prefix}codeblock go`).join("").trim();
             // Go code blocks format
-            message.channel.send(`Code shared by **${message.author.tag}**\n` +
-                '```go\n' + `${go}` + '```');
+            message.channel.send('```go\n' + `${userInput}` + '```');
         }
         if (message.content.startsWith(`${config.prefix}codeblock json`)){
-            // Store user input in a variable
-            let json = message.content.split(`${config.prefix}codeblock json`).join("").trim();
             // Json code blocks format
-            message.channel.send(`Code shared by **${message.author.tag}**\n` +
-                '```json\n' + `${json}` + '```');
+            message.channel.send('```json\n' + `${userInput}` + '```');
         }
         if (message.content.startsWith(`${config.prefix}codeblock lua`)){
-            // Store user input in a variable
-            let lua = message.content.split(`${config.prefix}codeblock lua`).join("").trim();
             // Lua code blocks format
-            message.channel.send(`Code shared by **${message.author.tag}**\n` +
-                '```lua\n' + `${lua}` + '```');
+            message.channel.send('```lua\n' + `${userInput}` + '```');
         }
         if (message.content.startsWith(`${config.prefix}codeblock perl`)){
-            // Store user input in a variable
-            let perl = message.content.split(`${config.prefix}codeblock perl`).join("").trim();
             // Perl code blocks format
-            message.channel.send(`Code shared by **${message.author.tag}**\n` +
-                '```perl\n' + `${perl}` + '```');
+            message.channel.send('```perl\n' + `${userInput}` + '```');
         }
         if (message.content.startsWith(`${config.prefix}codeblock python`)){
-            // Store user input in a variable
-            let python = message.content.split(`${config.prefix}codeblock python`).join("").trim();
             // Python code blocks format
-            message.channel.send(`Code shared by **${message.author.tag}**\n` +
-                '```python\n' + `${python}` + '```');
+            message.channel.send('```python\n' + `${userInput}` + '```');
         }
         if (message.content.startsWith(`${config.prefix}codeblock scss`)){
-            // Store user input in a variable
-            let scss = message.content.split(`${config.prefix}codeblock scss`).join("").trim();
             // SCSS code blocks format
-            message.channel.send(`Code shared by **${message.author.tag}**\n` +
-                '```scss\n' + `${scss}` + '```');
+            message.channel.send('```scss\n' + `${userInput}` + '```');
         }
     }
 }
